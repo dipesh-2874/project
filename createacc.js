c6a40abd8ce22cbac65fa187130b73f8a6c8a122
Cerebro-184 <mr.dipesh184@gmail.com>
2025-02-15 17:14:33 +0530

    updated

--- createacc.js
+++ createacc.js
@@ -3,10 +3,15 @@ document.getElementById("signupForm").addEventListener("submit", function(event)
 
     let name = document.getElementById("name").value.trim();
     let email = document.getElementById("email").value.trim();
+    let number = document.getElementById("phone").value.trim();
     let password = document.getElementById("password").value;
     let confirmPassword = document.getElementById("confirmPassword").value;
     if (name === "" || email === "" || password === "" || confirmPassword === "") {
         alert("All fields are required!")
+        return;
+    }
+    if (number <= 999999999 || number >= 9999999999){
+        alert("Invalid Number")
         return;
     }
     if (password !== confirmPassword) {
